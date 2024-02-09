a86477e767193e422f173544ce5b6802905a01be
Pierre Plourde <pierre@spartancsharp.net>
2024-02-09 12:26:17 -0500

    Create Code
    Build a WPF Application that has a simple data-entry sreen with First and Last Name fields. Have a button say "Hi {FN} {LN}" when pressed.

--- SayHi/MainWindow.xaml.cs
+++ SayHi/MainWindow.xaml.cs
@@ -24,5 +24,31 @@ namespace SayHi
 		{
 			InitializeComponent();
 		}
+
+		private void SayHiButton_Click(object sender, RoutedEventArgs e)
+		{
+			if ( string.IsNullOrWhiteSpace(firstNameTextBox.Text) && string.IsNullOrWhiteSpace(lastNameTextBox.Text) )
+			{
+				sayHiTextBlock.Text = "You must enter both a First Name and a Last Name to Say Hi!";
+				_ = firstNameTextBox.Focus();
+			}
+			else if ( string.IsNullOrWhiteSpace(firstNameTextBox.Text) )
+			{
+				sayHiTextBlock.Text = "You must enter a First Name to Say Hi!";
+				_ = firstNameTextBox.Focus();
+			}
+			else if ( string.IsNullOrWhiteSpace(lastNameTextBox.Text) )
+			{
+				sayHiTextBlock.Text = "You must enter a Last Name to Say Hi!";
+				_ = lastNameTextBox.Focus();
+			}
+			else
+			{
+				sayHiTextBlock.Text = $"Hi {firstNameTextBox.Text} {lastNameTextBox.Text}!";
+				firstNameTextBox.Text = "";
+				lastNameTextBox.Text = "";
+				_ = firstNameTextBox.Focus();
+			}
+		}
 	}
 }
